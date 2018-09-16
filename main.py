e29d5df3682d8b110fc3e5999d4f040305e4fce7
Backerich <25672920+Backerich@users.noreply.github.com>
2018-09-16 18:12:15 +0200

    ASCII symbols are implemented

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import random as rn
+import string
 
 class Picker:
-    lsit_of_ASCII = ["Hey", "2"]
+    lsit_of_ASCII = list(string.printable)
+
+    def __init__(self):
+        # Last ASCII signs are not really usefull
+        del self.lsit_of_ASCII[-6:]
 
     def pick_random(self):
         random_choice = rn.choice(self.lsit_of_ASCII)
         self.lsit_of_ASCII.remove(random_choice)
         return random_choice
+
 
 def main():
     p = Picker()
