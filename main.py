d0e28cbf628cc5e1b592c9bda7e60b5a40d2fba4
Backerich <25672920+Backerich@users.noreply.github.com>
2018-09-16 17:57:54 +0200

    Random picks are now implemented

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import random as rn
+
+class Picker:
+    lsit_of_ASCII = ["Hey", "2"]
+
+    def pick_random(self):
+        random_choice = rn.choice(self.lsit_of_ASCII)
+        self.lsit_of_ASCII.remove(random_choice)
+        return random_choice
+
 def main():
-    print("Hello Password-Generator")
+    p = Picker()
+    print(p.pick_random())
+    print(p.pick_random())
 
 if __name__ == '__main__':
     main()
