ba52e0b4ee7a50445879ffcea1db7222b8aebfde
Backerich <25672920+Backerich@users.noreply.github.com>
2018-09-17 15:48:32 +0200

    BUG FIX where Card (row/column) could not be bigger than (26/9)

--- main.py
+++ main.py
@@ -33,38 +33,50 @@ class Card:
         self.rescale_list(len(string.digits), number_of_lines_vertical, self.list_of_digits)
 
     def rescale_list(self, ascii_number, number_of_lines, list_of_content):
+        number_of_lines = number_of_lines
+        print(list_of_content)
         if number_of_lines > ascii_number:
-            number_of_lines = ascii_number-1
-        if number_of_lines != 0:
-            del list_of_content[-number_of_lines:]
+            number_of_lines = ascii_number
+        n = ascii_number - number_of_lines
+        if n != 0:
+            del list_of_content[-n:]
+        return list_of_content
 
     def field(self):
         field = []
         p = Picker()
+        counter = 0
+        # self.list_of_digits = list(string.digits)
         self.list_of_ascii_uppercase.insert(0, "/")
         for i in range(len(self.list_of_digits)):
             row = []
-            for j in range(len(self.list_of_ascii_uppercase)):
+            for j in range(len(self.list_of_ascii_uppercase)-1):
                 random_choice = p.pick_random()
                 row.append(random_choice)
-            row.append("\n")
+            row.insert(0, self.list_of_digits[counter])
+            counter += 1
             field.append(row)
+        counter = 0
         return field
 
+    # TODO: letztes "\n" entfernen damit in der Konsole schöner aussieht
     def print_card(self):
         field = self.field()
 
         print("--"*20)
+        top_row = " ".join(self.list_of_ascii_uppercase)
+        print(top_row)
         for i in field:
             row_string = " ".join(i)
             print(row_string)
+        print("--"*20)
 
 
 # class Generator:
 
-
+# Höher als 25,9 geht nicht -> 9 und Z werden nicht angezeigt
 def main():
-    c = Card(1, 1)
+    c = Card(26,10)
     c.print_card()
 
 if __name__ == '__main__':
